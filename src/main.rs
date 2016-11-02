98365d14145812c45bd8337498e47351af2262b8
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-01 22:35:54 -0700

    master: bktree works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate bk_tree;
+
+use bk_tree::{BKTree, metrics};
+
 fn main() {
-    println!("Hello, world!");
+    let mut bkt: BKTree<&str> = BKTree::new(metrics::levenshtein);
+
+    bkt.add("foo");
+    bkt.add("bar");
+    bkt.add("baz");
+    bkt.add("quux");
+    bkt.add("fooo");
+
+    println!(">>> 0 {:?}", bkt.find("bar", 0));
+    println!(">>> 1 {:?}", bkt.find("bar", 1));
+    println!(">>> 2 {:?}", bkt.find("bar", 2));
 }
