2620a20a38f8dab42351b8302fce0e1ca9e7849b
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-02 15:33:15 -0700

    master: store frequency data on separate structure for now

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 extern crate bk_tree;
-
 use bk_tree::{BKTree, metrics};
+
+use std::collections::HashMap;
 
 fn main() {
     let mut bkt: BKTree<&str> = BKTree::new(metrics::levenshtein);
+    let mut frq: HashMap<String,u64> = HashMap::new();
 
-    bkt.add("foo");
-    bkt.add("bar");
-    bkt.add("baz");
-    bkt.add("quux");
-    bkt.add("fooo");
+    bump("foo", &mut bkt, &mut frq);
+    bump("bar", &mut bkt, &mut frq);
+    bump("bar", &mut bkt, &mut frq);
+    bump("baz", &mut bkt, &mut frq);
+    bump("quux", &mut bkt, &mut frq);
+    bump("fooo", &mut bkt, &mut frq);
 
+    println!(">>> frq {}: {}", "bar", frq.get("bar").unwrap());
     println!(">>> 0 {:?}", bkt.find("bar", 0));
     println!(">>> 1 {:?}", bkt.find("bar", 1));
     println!(">>> 2 {:?}", bkt.find("bar", 2));
+}
+
+
+fn bump<'a>(s: &'a str, bkt: &mut BKTree<&'a str>, frq: &mut HashMap<String,u64>) -> () {
+    let ss = s.to_string();
+    match frq.get(&ss) {
+        Some(&freq) => {
+            frq.insert(ss, freq + 1);
+        }
+        _ => {
+            frq.insert(ss, 1);
+            bkt.add(s);
+        }
+    }
 }
