082ec441fb77e5e5835b80c4f1bd8659477cc400
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-03 23:57:41 -0700

    master: beginning of a words extractor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+#[macro_use]
+extern crate nom;
+
 extern crate bk_tree;
 use bk_tree::{BKTree, metrics};
 
 use std::collections::HashMap;
+
+use nom::{IResult,alpha,space};
+
+named!(words<&str>,
+       chain!(
+           space? ~
+           word: map_res!(alpha, std::str::from_utf8) ~
+           space?
+           ,|| word
+       )
+      );
+
 
 fn main() {
     let mut bkt: BKTree<&str> = BKTree::new(metrics::levenshtein);
@@ -18,6 +33,12 @@ fn main() {
     println!(">>> 0 {:?}", bkt.find("bar", 0));
     println!(">>> 1 {:?}", bkt.find("bar", 1));
     println!(">>> 2 {:?}", bkt.find("bar", 2));
+    match words("  yo!".as_bytes()) {
+        IResult::Done(rest, val) => {
+            println!("{:?} rest: {:?}", val, rest);
+        },
+        _ => println!("error!")
+    }
 }
 
 
@@ -32,4 +53,12 @@ fn bump<'a>(s: &'a str, bkt: &mut BKTree<&'a str>, frq: &mut HashMap<String,u64>
             bkt.add(s);
         }
     }
+}
+
+
+#[test]
+fn test_words_parser() {
+    let empty = "".as_bytes();
+    assert_eq!(words("hey".as_bytes()), IResult::Done(empty, "hey"));
+    assert_eq!(words(" hey".as_bytes()), IResult::Done(empty, "hey"));
 }
